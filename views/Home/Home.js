98969a988ad95b820cc3e15c53b55f8fa11a6d43
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-10-31 00:48:28 -0400

    form works, but no ajax yet

--- views/Home/Home.js
+++ views/Home/Home.js
@@ -2,10 +2,13 @@
 window.onload = function () {
     // add event listeners
     const pendingBtn = document.querySelector(".show-pending");
+    document.querySelector("#new-expense").addEventListener("submit", e => submitExpense(e))
     // pendingBtn.addEventListener("click", showPending);
-    console.log('pending button: ', pendingBtn)
     // renderExpenses();
-    
+
+    // for searching for an expense, ICEBOX
+    const expenseDictionary = new Map;
+
     const expensesList = document.querySelector(".expenses");
 
     // fetch the expenses
@@ -14,7 +17,7 @@ window.onload = function () {
     console.log('data; ', data)
 
     // render the expenses by putting each in a list tag and appending it to expensesList
-    function renderExpenses(bigFatArray)  {
+    function renderExpenses(bigFatArray) {
         bigFatArray.forEach(expense => {
             const approvedOrNot = expense.approved ? "Approved" : "Pending";
             const group = document.createElement("a");
@@ -28,48 +31,65 @@ window.onload = function () {
             heading.innerText = expense.name;
             const status = document.createElement("small");
             status.innerText = approvedOrNot;
-            
+
 
             card.appendChild(status);
             card.appendChild(heading);
             group.appendChild(card);
-            
+
             expensesList.appendChild(group);
         });
     }
 
     // when you click the pending button, 
-        // add event listener to onclick of 'Pending'
-        
-        
-        // only display pending 
-        
-    }
+    // add event listener to onclick of 'Pending'
 
-    function showAll() {
-        const expenses = document.querySelectorAll(".expense");
-        expenses.forEach(expense => {
-            expense.setAttribute("style", "display: show");
-        });
-    }
-            function showPending() {
-                console.log('clicked pending!')
-                const expenses = document.querySelectorAll(".expense");
-                expenses.forEach(expense => {
-                    console.log('running in foreach')
-                    if (!expense.classList.contains("Pending")) {
-                        expense.setAttribute("style", "display: none;");
-                    }
-                });
-            }
 
-            function showApproved() {
-                console.log('clicked pending!')
-                const expenses = document.querySelectorAll(".expense");
-                expenses.forEach(expense => {
-                    console.log('running in foreach')
-                    if (!expense.classList.contains("Approved")) {
-                        expense.setAttribute("style", "display: none;");
-                    }
-                });
-            }
+    // only display pending 
+
+}
+
+function showAll() {
+    const expenses = document.querySelectorAll(".expense");
+    expenses.forEach(expense => {
+        expense.setAttribute("style", "display: show");
+    });
+}
+function showPending() {
+    console.log('clicked pending!')
+    const expenses = document.querySelectorAll(".expense");
+    expenses.forEach(expense => {
+        console.log('running in foreach')
+        if (!expense.classList.contains("Pending")) {
+            expense.setAttribute("style", "display: none;");
+        }
+    });
+}
+
+function showApproved() {
+    console.log('clicked pending!')
+    const expenses = document.querySelectorAll(".expense");
+    expenses.forEach(expense => {
+        console.log('running in foreach')
+        if (!expense.classList.contains("Approved")) {
+            expense.setAttribute("style", "display: none;");
+        }
+    });
+}
+
+function submitExpense(event) {
+    event.preventDefault();
+    const expenseForWhat = document.querySelector(".new-expense-title").value;
+    const howMuch = document.querySelector(".how-much").value;
+
+    console.log(`Expense   [ ${expenseForWhat}  ${howMuch} ]`);
+
+    const toAnimate = document.querySelector(".alert-success");
+    toAnimate.classList.remove("not-yet-submitted");
+
+    let fields = document.querySelectorAll('input[type="text"]');
+    fields.forEach(field => field.value = '');
+
+    setTimeout(() => toAnimate.setAttribute("class", "not-yet-submitted"), 5000);
+
+}
