47f253d95e6afef8afec4879ac1206e137cdd2eb
Pantherhawk <alexrosenthalster@gmail.com>
2018-11-04 12:39:06 -0800

    feature/ employee list from db renders on manager sidebar

--- src/main/webapp/js/Manager.js
+++ src/main/webapp/js/Manager.js
@@ -4,9 +4,31 @@ window.onload = function() {
     const employeesContainer = document.querySelector("#employee-list");
     // fetch the list of all employees from DB
     const employees = EmployeeRepository.fetchAll()
-        .then(employees => {
-            console.log("employee list got to Manager code................" + employees)
+        .then(data => {
+            console.log("employee list got to Manager code................" + JSON.stringify(data));
+            return data;
         })
-    // iterate over employee list 
-        // render the employee
+        .then(employees => {
+          console.log("employee list    " + employees )
+          if (Array.isArray(employees)) {
+            // iterate over employee list 
+            for (let employee of employees) {
+              const empEl = document.createElement("li");
+              const badge = document.createElement("span");
+              empEl.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center");
+              badge.setAttribute("class", "badge badge-primary badge-pill")
+      
+              // fill element with the employee data using HTML5 data attribute
+              for (let key in employee) {
+                empEl.setAttribute(`data-${key}`, ""+employee[key]);
+              }
+              // get the name
+              const fullName = employee.firstName + " " + employee.lastName;
+              empEl.appendChild(document.createTextNode(fullName));
+              console.log("employee li to append ---------->" + empEl)
+              // render the employee
+              employeesContainer.appendChild(empEl);
+            }
+          }
+        });
 }
