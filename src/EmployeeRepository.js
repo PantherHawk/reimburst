6065ac4353677a80df45304300d0104aabed114c
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-11-06 07:24:07 -0500

    bug / type error when adding id attribute to expense element in manager view

---
+++ src/EmployeeRepository.js
@@ -0,0 +1,51 @@
+// const API = require('../../util/API');
+
+const ExpensesRepository = {
+    uri: {
+        forEveryone: `/expenses`,
+        forUser: `expenses/user/:`,
+    },
+    _normalizeData(expenses) {
+        console.log('expenses: ', expenses)
+        
+        return expenses.map(expense => {
+            const { id, name, status, amount, dateHandled, dateSubmitted, description, employeeId, teamId } = expense;
+            return {
+                id: id,
+                title: name,
+                status: status == null ? 'Pending' : (status == 'APPROVED' ? 'Approved' : 'Rejected'),
+                amount: amount,
+                daysSinceRequest: diffDays(new Date(), new Date(dateSubmitted)),
+                dateHandled: dateHandled,
+                description: description,
+                employee_id: employeeId,
+                team_id: teamId,
+            }
+        })
+    },
+    // add uri later
+    getForUser(user) {
+        return API.get(`/api/expenses/`);
+    },
+    // add uri later
+    getAll() {
+        return API.get(this.uri.forEveryone)
+        .then(this._normalizeData);
+    },
+
+    approveExpense(decision, id) {
+    	decision = decision.toLowerCase();
+        return API.post(`/decision/decision?decision=${decision}`, {"id": id})
+        .then(success => success)
+        .catch(err => console.log('Err: ', err))
+    }
+
+
+}
+
+const diffDays = (past, now) => {
+    const oneDay = 24*60*60*1000;
+    return Math.round(Math.abs(now.getTime() - past.getTime())/(oneDay));
+}
+
+// module.exports = ExpensesRepository;
--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -4,19 +4,25 @@
         <ul id="employees">
             <li v-bind:key="employee.id" v-for="employee of employees">
                 <div v-bind:id="employee.id">
-                    <input type="radio" v-bind:id="employee.id" v-bind:value="employee.id" v-model="employeeSelected"><label for="employee.id">{{ employee.name }}</label>
+                    <input type="radio" v-bind:id="employee.id" v-bind:value="employee.id" v-model="employeeSelected"><label for="employee.id">{{ employee.username }}</label>
                 </div>
             </li>
         </ul>
         <ul id="expenses">
             <li v-bind:key="item.id" v-for="item of filteredItems">
-                {{ item.name }} - {{ item.amount }} - {{ item.status }}
+                <button v-on:click="approveOrReject">Approve</button>
+                <button v-on:click="approveOrReject">Reject</button>
+                {{ item.title }} - {{ item.id }} - {{ item.userId }}
             </li>
         </ul>
     </div>
 </template>
 <script>
 export default {
+    created: function() {
+        this.getExpenses()
+        this.getEmployees()
+    },
     data() {
         return {
             employees: [
@@ -46,7 +52,30 @@ export default {
     },
     methods: {
         showAll() {
-            console.log()
+            this.employeeSelected = null;
+
+        },
+        getExpenses() {
+            // TODO: fetch from Servlet, right now it's just jsonplaceholder
+            fetch(`https://jsonplaceholder.typicode.com/albums`)
+            .then(res => res.json())
+            .then(expenses => this.expenses = expenses)
+            .catch(err => console.log(err))
+        },
+        getEmployees() {
+            // TODO: fetch from Servlet, right now it's just jsonplaceholder
+            fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => res.json())
+            .then(result => {
+                console.log('uers from jsonplaceholder    ' + JSON.stringify(result))
+                return result
+            })
+            .then(employees => this.employees = employees)
+            .catch(err => console.log(err))
+        },
+        approveOrReject($event) {
+            console.log("event parent:   ", $event.target.parentNode.key)
+            this.decision = $event.target.innerText
 
         }
     }
